4e6fa9e0c2dedd40647cfaa60fa15bcabdb8ace5
kumardeep0060 <56909475+kumardeep0060@users.noreply.github.com>
2021-05-05 10:50:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "aws_terraform_jenkins" {
-  ami             = "${data.aws_ami.ubuntu.id}"
+  ami             = "ami-0bcf5425cdc1d8a85"
   instance_type   = "${var.instance_type}"
   key_name        = "${var.key_name}"
   
